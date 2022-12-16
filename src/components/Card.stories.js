3e2836d38fc7160ba9fdebe4260d9aff891cbdaf
Makoto Onohara <makonobumakonobu@gmail.com>
2022-12-16 17:12:49 +0900

    add Card components and its storybook script

---
+++ src/components/Card.stories.js
@@ -0,0 +1,31 @@
+// Card.stories.js
+
+import Card from "./Card.vue";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/vue/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Card",
+  component: Card,
+};
+
+//👇 We create a “template” of how args map to rendering
+const Template = (args) => ({
+  components: { Card },
+  setup() {
+    //👇 The args will now be passed down to the template
+    return { args };
+  },
+  template: '<Card v-bind="args"/>',
+});
+
+export const FirstStory = Template.bind({});
+
+FirstStory.args = {
+  /* 👇 The args you need here will depend on your component */
+  title: "Primary",
+  subText: "Secondary",
+};
